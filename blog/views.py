82f4a00fe961bafea1a81972ff76875ae86cb273
Juan Bautista Arambarri <jbat1997@gmail.com>
2025-10-16 16:52:59 -0300

    blog templates html and main css
    Learning about templates, how to load/render and display data on it.
    Template inheritance to clean up htmls and using bootstrap and static files.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Juan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 16, 2025'
+    },
+    {
+        'author': 'Fer',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 17, 2025'
+    }
+]
+
 # Create your views here.
 def home(request): #request argument has to be here
-    return HttpResponse('<h1>Blog Home</h1>')
+    context= { #Dictionary that has a dictionary with posts
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) #will let us access "context" dictionary.
+    #return HttpResponse('<h1>Blog Home</h1>')
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title': 'About'}) #can do context directly here(dict)
 
