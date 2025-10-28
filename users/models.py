6e246d84d2fb4390d8b826799e5d049e933d22ed
Juan Bautista Arambarri <jbat1997@gmail.com>
2025-10-28 09:29:57 -0300

    User Profile and Picture implementation
    Adding a feature to upload a profile picture and adding django signals

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image= models.ImageField(default='default.jpg', upload_to='profile_pics') #profile_pics is a directory
+    ###Cascade -> if user is deleted, profile is also deleted. If profile is deleted, user is not.
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
 # Create your models here.
