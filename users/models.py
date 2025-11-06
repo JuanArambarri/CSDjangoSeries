20b7209b5f90a4786c81c792dec55f612a36a5eb
Juan Bautista Arambarri <jbat1997@gmail.com>
2025-11-06 09:36:27 -0300

    Implement update profile form
    Added a profile form to update username, picture and email. Additional features were added such as round pictures beside usernames and image resizing

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,3 +11,11 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
 # Create your models here.
+    def save(self): #run after our model is saved
+        super().save() #run parent save
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
