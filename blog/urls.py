9f2431a3ca39809262068b7029dddfd8ca2415be
Juan Bautista Arambarri <jbat1997@gmail.com>
2025-11-26 09:34:44 -0300

    Implementing pagination and post by user
    handling logic and links for users, profiles and so on.

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView
 )
 from . import views #. imports from this folder
 
@@ -12,7 +13,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'), #empty path is homepage, path naming avoids confusion
     path('about/',views.about, name='blog-about'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-
+    path('user/<str:username>',UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #int:pk requests an int posting key
